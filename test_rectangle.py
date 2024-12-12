41a38850892343a33c280840e8502f758d4e569f
Ярослав Прудий <aroslavprudij@Noutbuk-Aroslav.local>
2024-12-12 06:51:20 +0300

    1

---
+++ test_rectangle.py
@@ -0,0 +1,33 @@
+import unittest
+
+from rectangle import area, perimeter
+
+class TestRectangle(unittest.TestCase):
+    def test_area_positive(self):
+        self.assertEqual(area(3, 5), 15)
+        self.assertEqual(area(10, 10), 100)
+
+    def test_area_zero(self):
+
+        self.assertEqual(area(0, 5), 0)
+        self.assertEqual(area(0, 0), 0)
+
+
+
+    def test_perimeter_positive(self):
+
+        self.assertEqual(perimeter(5, 3), 16)
+        self.assertEqual(perimeter(10, 10), 40)
+
+    def test_perimeter_zero(self):
+
+        self.assertEqual(perimeter(0, 5), 10)
+        self.assertEqual(perimeter(0, 0), 0)
+
+    def test_perimeter_zero(self):
+
+        self.assertEqual(perimeter(0, -5), 10)
+        self.assertEqual(perimeter(0, 0), 0)
+
+if __name__ == "__main__":
+    unittest.main()
