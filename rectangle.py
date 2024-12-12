1894d414e060d0e8c81b7ebbe3cc86a682e57b10
Ярослав Прудий <aroslavprudij@Noutbuk-Aroslav.local>
2024-12-12 09:58:59 +0300

    4

---
+++ rectangle.py
@@ -0,0 +1,17 @@
+
+def area(length, width):
+    """
+    Вычисляет площадь прямоугольника.
+    """
+    if length < 0 or width < 0:
+        raise ValueError("Длины сторон должны быть неотрицательными.")
+    return length * width
+
+
+def perimeter(length, width):
+    """
+    Вычисляет периметр прямоугольника.
+    """
+    if length < 0 or width < 0:
+        raise ValueError("Длины сторон должны быть неотрицательными.")
+    return 2 * (length + width)
